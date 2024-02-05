a639e476f7c60945bd6458a813ca0d7b706b70e8
dasdeo <mark@anio.software>
2024-02-05 12:40:41 +0100

    Give every worker a unique id.

--- src/index.mjs
+++ src/index.mjs
@@ -2,11 +2,13 @@ import {createMasterInterface} from "@anio-js-foundation/master-slave-protocol"
 import nodeCreateWorker from "@anio-js-foundation/node-create-worker"
 import browserCreateWebWorker from "@anio-js-foundation/browser-create-web-worker"
 import createTemporaryResource from "@anio-js-foundation/create-temporary-resource"
+import createRandomIdentifier from "@anio-js-core-foundation/create-random-identifier"
 import isNode from "@anio-js-core-foundation/is-node"
 
 export default async function(
 	request_handler, request_handler_export_name = "requestHandler", worker_options = {}
 ) {
+	const worker_id = createRandomIdentifier(32)
 	const createWorker = isNode() ? nodeCreateWorker : browserCreateWebWorker
 
 	const bootstrap = await createTemporaryResource(
@@ -26,6 +28,8 @@ export default async function(
 	bootstrap.cleanup()
 
 	return {
+		id: worker_id,
+
 		sendRequest(...args) {
 			return master.sendRequest(...args)
 		},
